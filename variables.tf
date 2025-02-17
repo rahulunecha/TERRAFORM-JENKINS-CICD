b1ec72cfb3f365bb2fe7d6f8f379d74a9d5d2789
Ra}{ul Unecha <runecha08@gmail.com>
2025-02-17 14:38:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-south-1"
+  default     = "eu-north-1"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "Mumbai"
+  default     = "newrahul"
 }
 variable "instance_type" {
   description = "instance type for ec2"
-  default     = "t2.medium"
+  default     = "t3.medium"
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0f5ee92e2d63afc18"
+  default     = "ami-09a9858973b288bdd"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "mrcloudbook-777-ajaykumar-yegireddi"
+  default     = "tfsafestore"
 }
 
 variable "acl" {
